69f01bdcb20ab69790123996d268b1f8b72ea52e
Alexander <fokke_wolf@mail.ru>
2016-09-06 11:47:42 +0300

    work on logs

---
+++ Presentation/ViewModels/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Presentation.ViewModels
+{
+    public class Log
+    {
+        public DateTime EventLogTime { get; private set; }
+        public Level LevelType { get; private set; }
+        public string Description { get; private set; }
+
+        public Log(DateTime EventLogTimeParam, Level LevelTypeParam, string DescriptionParam)
+        {
+            EventLogTime = EventLogTimeParam;
+            LevelType = LevelTypeParam;
+            Description = DescriptionParam;
+        }
+    }
+
+    public enum Level
+    {
+        Critical,
+        Error,
+        Attention,
+        Success,
+        Info
+    }
+}
